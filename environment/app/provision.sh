2f159dacb25e5d6d50cffeecbcfe30dbd6cc7b75
Saheed Lamina git config --global user.email you@example.comgit config --global user.name Saheed <agbolamina@hotmail.com>
2020-07-31 01:05:55 +0100

    Complete functional code

---
+++ environment/app/provision.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Update the sources list
+sudo apt-get update -y
+
+# upgrade any packages available
+sudo apt-get upgrade -y
+
+# install nginx
+sudo apt-get install nginx -y
+
+# install git
+sudo apt-get install git -y
+
+# install nodejs
+sudo apt-get install python-software-properties
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+sudo apt-get install nodejs -y
+
+# install npm 
+npm install
+
+# install pm2
+sudo npm install pm2 -g
+
+# Configuring nginx proxy
+sudo unlink /etc/nginx/sites-enabled/default
+sudo ln -s /home/ubuntu/environment/default /etc/nginx/sites-enabled/default
+sudo systemctl start nginx
+sudo systemctl stop nginx
+sudo systemctl restart nginx
+sudo systemctl status nginx
